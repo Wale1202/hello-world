1f1df7fc255411940df2936aa10bc6a8a02d3ae1
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2023-06-21 06:32:08 +0100

    Add files via upload

---
+++ newCalculator.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void opera(double num1, double num2, char op){
+
+
+
+if(op == '+'){
+printf("%f", num1+num2);
+ }else if(op == '-'){
+ printf("%f", num1-num2);
+ }else if(op == '/'){
+ printf("%f", num1/num2);
+ }else if(op == '*'){
+ printf("%f", num1*num2);
+ }else{
+ printf("Try another operator");
+ };
+
+}
+
+
+int main(){
+
+double num1;
+double num2;
+char op;
+double answer;
+
+answer =
+
+printf("Enter number 1: ");
+scanf("%lf", &num1);
+
+
+printf("Enter operator: ");
+scanf(" %c", &op);
+
+
+printf("Enter number 2: ");
+scanf("%lf", &num2);
+
+opera(num1, num2, op);
+
+
+   return 0;
+}
